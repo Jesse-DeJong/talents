cfe6d07a4dca104d125553a7e6aba06659c5c9d1
Jesse-DeJong <jdoceau@gmail.com>
2023-03-17 20:14:05 +1100

    progress on programatically building tree

--- index.js
+++ index.js
@@ -1,32 +1,80 @@
+import { talentTree } from "./tree.js";
+
 // DOM Targeting
 const root = document.getElementById("root");
 const points = document.getElementsByClassName("points");
+const elementDetails = ["div", "talent", ""]
 
-const buildTalentTree = (rows) => {
+const buildTalentTree = (talentTree) => {
     
-    root.append(tree)
-    
-    rows.map( row => {
-        root.append(row);
+    buildTree();
+    let parentTree = document.getElementById("tree");
+
+    Object.entries(talentTree).map(talentRow => {
+        buildTalentRow
     })
+    buildTalentRow
+
+    Object.entries(talentTree).map(i => {
+        buildTalent(elementDetails[0], elementDetails[1], elementDetails[3], i.name, parentTree);
+    })
+    
 }
 
-const buildTalentRow = () => {
-
+const buildTalentRow = (element, classes, styles, content, parent) => {
+    let newEl = document.createElement(element);
+    if(classes){
+        newEl.className = classes;
+    }
+    if(styles){
+        newEl.style = styles;
+    }
+    if(content){
+        let textNode = document.createTextNode(content);
+        newEl.appendChild(textNode);
+    }
+    parent.appendChild(newEl);
+    
 }
 
-//const htmlBuilder = (tag, class) => {
-//    
+const buildTalent = (element, classes, styles, content, parent) => {
+    let newEl = document.createElement(element);
+    if(classes){
+        newEl.className = classes;
+    }
+    if(styles){
+        newEl.style = styles;
+    }
+    if(content){
+        let textNode = document.createTextNode(content);
+        newEl.appendChild(textNode);
+    }
+    parent.appendChild(newEl);
+}
+    
+const buildTree = () => {
+    
+    let tree = document.createElement("div");
+    tree.className = "tree";
+    tree.id = "tree";
+    root.appendChild(tree)
+}
+    
+
+//const buildElement2 = () => {
+//    return `<${element} class="${class}" style=${style}>${content}</${element}>`;
 //}
 
-switch(e.target.Any(classList.contains())){
-    case onepoints:
-        classlist.remove("onepoints");
-        classlist.add("twopoints");
-}
+//switch(e.target.Any(classList.contains())){
+//    case onepoints:
+//        classlist.remove("onepoints");
+//        classlist.add("twopoints");
+//}
 
 const setPoints = () => {
     points.map( p => {
         p.text 
     })
 }
+
+buildTalentTree(talentTree)
---
+++ tree.js
@@ -0,0 +1,73 @@
+export const talentTree = {
+    row1: {
+        t1: {
+            name: "TestTalent1",
+            points: 5,
+            desc: "testestest",
+            row: 1,
+            col: 1
+        },
+        t2: {
+            name: "TestTalent2",
+            points: 5,
+            desc: "testing",
+            row: 1,
+            col: 2
+        },
+        t3: {
+            name: "TestTalent3",
+            points: 5,
+            desc: "test123test",
+            row: 1,
+            col: 3
+        }
+    },
+    row2: {
+        t1: {
+            name: "TestTalent1",
+            points: 5,
+            desc: "testestest",
+            row: 1,
+            col: 1
+        },
+        t2: {
+            name: "TestTalent2",
+            points: 5,
+            desc: "testing",
+            row: 1,
+            col: 2
+        },
+        t3: {
+            name: "TestTalent3",
+            points: 5,
+            desc: "test123test",
+            row: 1,
+            col: 3
+        }
+    },
+    row3: {
+        t1: {
+            name: "TestTalent1",
+            points: 5,
+            desc: "testestest",
+            row: 1,
+            col: 1
+        },
+        t2: {
+            name: "TestTalent2",
+            points: 5,
+            desc: "testing",
+            row: 1,
+            col: 2
+        }
+    },
+    row4: {
+        t1: {
+            name: "TestTalent1",
+            points: 5,
+            desc: "testestest",
+            row: 1,
+            col: 1
+        }
+    }
+};

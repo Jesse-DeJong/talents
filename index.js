2144801d2a5f37fd48ab69a75882b3628381c81b
Jesse-DeJong <jdoceau@gmail.com>
2023-03-17 19:12:48 +1100

    minor initial work

---
+++ index.js
@@ -0,0 +1,32 @@
+// DOM Targeting
+const root = document.getElementById("root");
+const points = document.getElementsByClassName("points");
+
+const buildTalentTree = (rows) => {
+    
+    root.append(tree)
+    
+    rows.map( row => {
+        root.append(row);
+    })
+}
+
+const buildTalentRow = () => {
+
+}
+
+//const htmlBuilder = (tag, class) => {
+//    
+//}
+
+switch(e.target.Any(classList.contains())){
+    case onepoints:
+        classlist.remove("onepoints");
+        classlist.add("twopoints");
+}
+
+const setPoints = () => {
+    points.map( p => {
+        p.text 
+    })
+}
